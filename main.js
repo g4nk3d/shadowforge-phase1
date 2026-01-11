71caee9d07bb5378299487362a4924aac37a9b8e
g4nk3d <gimmeurlife@gmail.com>
2026-01-11 02:29:28 -0500

    Update main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ player.position.set(0, 1, 0);
 scene.add(player);
 
 // === CAMERA CONTROLS ===
-const controls = new OrbitControls(camera, renderer.domElement);
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.target.set(0, 1, 0);
 controls.update();
 
@@ -57,6 +57,7 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
 
 
 

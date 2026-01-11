e66b040237d88436bc456c8697f37d6e94ee95b6
g4nk3d <gimmeurlife@gmail.com>
2026-01-11 00:55:33 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // === THREE.JS BASIC SETUP ===
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x0f0f0f); // Mordor sky
+scene.background = new THREE.Color(0x222222); // or try 0x111111
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 camera.position.set(0, 10, 20);
@@ -56,5 +56,6 @@ function animate() {
   renderer.render(scene, camera);
 }
 animate();
+
 
 
